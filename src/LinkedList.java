64f713555f72411e8c76935b6a3fbd352b18c576
Chanwoo <titoddd@naver.com>
2022-09-19 10:55:54 +0900

    LinkedList add&remove

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -139,14 +139,38 @@ public class LinkedList {
 		}
 		
 		public Object next() {
-			lastReturned = next;
+			lastReturned = next; // 한번이라도 next()를 수행했다면 lastReturned가 존재할것이고,그게바로 선택된 노트이다.
 			next = next.next;
-			nextIndex++;
+			nextIndex++; // next()를 수행한 횟수 -> hasNext()구현을 위해 세어줘 한다.
 			return lastReturned.data;
 		}
 		// singly linkedList 에서는 previous와 같은 기능 구현이 불가능하다. 왜냐하면, next로 다음노드는 알 수 있지만 앞 노드는 알 방법이 없기 때문.
 		public boolean hasNext() {
-			return nextIndex < size();
+			return nextIndex < size(); // nextIndex=size()  인 경우에는 next=null이다.
+		}
+		public void add(Object input) {
+			Node newNode = new Node(input);
+			//first input
+			
+			// 맨 처음 add 와 중간에 add 하는것을 구분해주어야 한다. 
+			// 어떻게? lastReturned의 값의 존재유무로 해주겠다. ( 있다는것은 next()를 수행했다는것 -> 중간이란뜻, 없다면 next()를 한번도 수행하지 않았다는것-> 맨 처음이란뜻 )
+			if(lastReturned == null) {
+				head = newNode;
+				newNode.next = next;
+			}else if(lastReturned != null) {
+				lastReturned.next = newNode;
+				newNode.next = next.next;
+			}
+			lastReturned = newNode; // 항상 새로 넣은 노드를 할당하기.
+			nextIndex++; //새로운 노드가 앞이든 뒤든 추가 될 때마다 더해주기.
+			size++;
+		}
+		public void remove() {
+			if(nextIndex == 0 ) {
+				throw new IllegalStateException();
+			}
+			LinkedList.this.remove(nextIndex-1); // 지우고자 하는 타겟은 lastReturned 노드이다. ( 선택된 노드 )
+			nextIndex--;
 		}
 	}
 	
--- src/LinkedListApp.java
+++ src/LinkedListApp.java
@@ -4,19 +4,26 @@ public class LinkedListApp {
 	public static void main(String[] args) {
 		
 		LinkedList numbers = new LinkedList();
-		numbers.addFirst(10);
-		numbers.addFirst(20);
-		numbers.addFirst(30);
-		/*
+		numbers.addLast(10);
+		numbers.addLast(20);
 		numbers.addLast(30);
-		numbers.addLast(30);
-		numbers.addLast(30);
-		*/
+		
 		LinkedList.ListIterator i = numbers.listIterator();
-		while( i.hasNext()) {
-			System.out.println(i.next());
+		i.add(5); // [5 , 10 , 20 , 30]
+		//i.next();
+		//i.add(15);
+		while(i.hasNext()) { //좋은 방법은 아니다. 따라서 Doubly LinkedList가 필요한것(prev로 서로연결됨)
+			if( (int)i.next() == 20) {
+				i.remove();
+			}
 		}
+		System.out.println(numbers);
+		
+		
+		
 		
 	}
+	
+
 
 }
